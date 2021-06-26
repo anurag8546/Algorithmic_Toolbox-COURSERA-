0cc542ce4a1379096877e9715b0ae32f97a61737
Anurag Mishra <76533034+anurag8546@users.noreply.github.com>
2021-06-27 04:07:46 +0530

    Create aplusb.cpp

---
+++ week-1/aplusb.cpp
@@ -0,0 +1,12 @@
+/*
+author:-Anurag Mishra(2:22AM)
+week-1(problem 1)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a;
+    int b;
+   cin>>a>>b;
+    cout<<a+b<<endl;
+}
