852a3c895705082af1e3d4efc3bfa9b5c3c8d945
Anurag Mishra <76533034+anurag8546@users.noreply.github.com>
2021-06-27 11:33:52 +0530

    Create gcd.cpp

---
+++ week-2/gcd.cpp
@@ -0,0 +1,16 @@
+/*
+author:-Anurag Mishra
+problem:-algo_tb(week2(problem-3))
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a , int b) {
+	if (a == 0)
+		return b;
+	return gcd(b%a,a);
+}
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << gcd(a, b) << endl;
+}
